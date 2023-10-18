7fd22f74b3d69c5d862eb7c4d05b1dd6862f23ae
Shoaib Akbar <shoaibjakhar11@gmail.com>
2023-10-18 13:45:15 +0500

    extracted php database class for db connection, added config.php file to make project more envirenment and configurations flexible and removed sql injection vulnerabilities

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 
-require 'router.php';
+// require 'router.php';
+
+$config = require 'config.php';
+$db = new Database($config['database']);
+
+$id = $_GET['id']; 
+
+$query = "Select * from posts where id = ?";
+$post = $db->query($query, [$id])->fetch();
+
+dd($post);
