55fbf97970c99384a65dc0f19dfd5d3275989428
Shoaib Akbar <shoaibjakhar11@gmail.com>
2023-12-04 12:35:05 +0500

    Composer and free autoloading, installed 2 packages collections and pest

---
+++ public/playground.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Collection;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$numbers = new Collection([
+    1,2,3,4,5,6,7,8,9,10
+]);
+
+// if($numbers->contains(10)) {
+//     echo "It contains 10";
+// }
+
+// if($numbers->contains(100)) {
+//     echo "It contains 10";
+// }
+
+// $less_then_and_equal_to_5 = $numbers->filter(function($numbers){
+//     return $numbers <= 5;
+// });
+
+// var_dump($less_then_and_equal_to_5);
